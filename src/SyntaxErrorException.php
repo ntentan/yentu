33a67821cb76034560037f81a6e91449f753585d
Ekow Abaka Ainooson <jainooson@gmail.com>
2014-09-28 13:16:53 +0000

    All exceptions can now report file numbers

--- src/SyntaxErrorException.php
+++ src/SyntaxErrorException.php
@@ -1,18 +1,7 @@
 <?php
 namespace yentu;
 
-class SyntaxErrorException extends \Exception
+class SyntaxErrorException extends YentuException
 {
-    public function __construct($message)
-    {
-        parent::__construct($message);
-        foreach($this->getTrace() as $item)
-        {
-            if(realpath(Yentu::getPath('migrations')) === dirname($item['file']))
-            {
-                $this->message .= " on line {$item['line']} of {$item['file']}";
-                break;
-            }
-        }
-    }
+
 }
---
+++ src/YentuException.php
@@ -0,0 +1,18 @@
+<?php
+namespace yentu;
+
+class YentuException extends \Exception
+{
+    public function __construct($message)
+    {
+        parent::__construct($message);
+        foreach($this->getTrace() as $item)
+        {
+            if(realpath(Yentu::getPath('migrations')) === dirname($item['file']))
+            {
+                $this->message .= " on line {$item['line']} of {$item['file']}";
+                break;
+            }
+        }
+    }
+}
