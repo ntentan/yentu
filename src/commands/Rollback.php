a0c8738b89016d06575d5299e38ab5084059f67a
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-11-17 20:48:59 +0000

    Added the ability to rollback specific migrations

--- src/commands/Rollback.php
+++ src/commands/Rollback.php
@@ -9,6 +9,9 @@ use yentu\Yentu;
 
 class Rollback implements \clearice\Command
 {
+    private $schemaCondition;
+    private $schemaConditionData = [];
+    
     public function run($options=array()) 
     {
         Yentu::greet();
@@ -17,12 +20,23 @@ class Rollback implements \clearice\Command
         ChangeReverser::setDriver($db);
         $previousMigration = '';
         
-        $session = $db->getLastSession();
+        if(isset($options['default-schema'])) {
+            $this->schemaCondition = "default_schema = ?";
+            $this->schemaConditionData[] = $options['default-schema'];
+        }
         
-        $operations = $db->query(
-            'SELECT id, method, arguments, migration, default_schema FROM yentu_history WHERE session = ? ORDER BY id DESC',
-            array($session)
-        );
+        if(empty($options)) {
+            $session = $db->getLastSession();
+            $operations = $db->query(
+                "SELECT id, method, arguments, migration, default_schema FROM yentu_history WHERE $this->schemaCondition session = ? ORDER BY id DESC",
+                $this->schemaConditionData + [$session]
+            );
+        } else {
+            $operations = [];
+            foreach($options['stand_alones'] as $set) {
+                $operations += $this->getOperations($db, $set);
+            }
+        }
         
         foreach($operations as $operation)
         {
@@ -37,5 +51,22 @@ class Rollback implements \clearice\Command
             ChangeReverser::call($operation['method'], json_decode($operation['arguments'], true));
             $db->query('DELETE FROM yentu_history WHERE id = ?', array($operation['id']));
         }
+    }
+    
+    private function getOperations($db, $set)
+    {
+        $operations = [];
+        if(preg_match("/[0-9]{14}/", $set)) {
+            $operations = $db->query(
+                "SELECT id, method, arguments, migration, default_schema FROM yentu_history WHERE $this->schemaCondition version = ? ORDER by id DESC",
+                $this->schemaConditionData + [$set]
+            );
+        } elseif (preg_match("/[a-zA-Z\_\-]+/", $set)) {
+            $operations = $db->query(
+                "SELECT id, method, arguments, migration, default_schema FROM yentu_history WHERE $this->schemaCondition migration = ? ORDER by id DESC",
+                $this->schemaConditionData + [$set]
+            );
+        }   
+        return $operations;
     }
 }
