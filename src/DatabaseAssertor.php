5d500a16eb7896c1802267911b4636da6ffe1990
James Ekow Abaka Ainooson <jainooson@gmail.com>
2024-11-29 22:35:02 -0600

    Slight code formatting

--- src/DatabaseAssertor.php
+++ src/DatabaseAssertor.php
@@ -5,97 +5,92 @@ namespace yentu;
 class DatabaseAssertor
 {
     private $description;
-    
+
     public function __construct($description)
-    {        
+    {
         $this->description = $description;
     }
-    
+
     public function doesSchemaExist($details)
     {
         return isset($this->description['schemata'][$details]);
     }
-    
+
     public function doesTableExist($details)
     {
-        if(is_string($details))
-        {
+        if (is_string($details)) {
             $details = array(
                 'schema' => false,
                 'name' => $details
             );
         }
-        
-        return $details['schema'] == false? 
-            isset($this->description['tables'][$details['name']])  : 
+
+        return $details['schema'] == false ?
+            isset($this->description['tables'][$details['name']]) :
             isset($this->description['schemata'][$details['schema']]['tables'][$details['name']]);
     }
-    
+
     public function doesColumnExist($details)
     {
         $table = $this->getTableDetails($details['schema'], $details['table']);
-        return isset($table['columns'][$details['name']]) ? 
+        return isset($table['columns'][$details['name']]) ?
             Parameters::wrap($table['columns'][$details['name']]) : false;
     }
-    
+
     private function doesItemExist($details, $type)
     {
         $table = $this->getTableDetails($details['schema'], $details['table']);
-        if(isset($details['columns']))
-        {
+        if (isset($details['columns']) && !empty($details['columns'])) {
             return isset($table["flat_$type"][$details['columns'][0]]) ? $table["flat_$type"][$details['columns'][0]] : false;
-        }
-        else if(isset($details['name']))
-        {
+        } else if (isset($details['name'])) {
             return isset($table[$type][$details['name']]) ? $table[$type][$details['name']] : false;
-        }        
+        }
     }
-    
+
     public function doesForeignKeyExist($details)
     {
         return $this->doesItemExist($details, 'foreign_keys');
     }
-    
+
     public function doesUniqueKeyExist($details)
     {
         return $this->doesItemExist($details, 'unique_keys');
     }
-    
+
     public function doesPrimaryKeyExist($details)
     {
         return $this->doesItemExist($details, 'primary_key');
-    } 
-    
+    }
+
     public function doesIndexExist($details)
     {
         return $this->doesItemExist($details, 'indices');
     }
-    
+
     public function doesViewExist($details)
     {
-        if(is_string($details))
-        {
+        if (is_string($details)) {
             $details = array(
                 'schema' => false,
                 'name' => $details
             );
         }
-        
+
         // too complex 
-        if($details['schema'] == false) {
-            return isset($this->description['views'][$details['name']]) ? $this->description['views'][$details['name']]['definition'] : false ;
+        if ($details['schema'] == false) {
+            return isset($this->description['views'][$details['name']]) ? $this->description['views'][$details['name']]['definition'] : false;
         } else {
             return (isset($this->description['schemata'][$details['schema']]['views'][$details['name']]) ?
                 $this->description['schemata'][$details['schema']]['views'][$details['name']]['definition'] : false);
         }
     }
-    
+
     private function getTableDetails($schema, $table)
     {
-        if($schema) {
+        if ($schema) {
             return $this->description['schemata'][$schema]['tables'][$table];
         } else {
             return $this->description['tables'][$table];
         }
-    }    
+    }
 }
--- src/manipulators/Postgresql.php
+++ src/manipulators/Postgresql.php
@@ -7,59 +7,69 @@ use yentu\Parameters;
 class Postgresql extends AbstractDatabaseManipulator
 {
 
-    private function buildTableName($name, $schema) {
+    private function buildTableName($name, $schema)
+    {
         return ($schema === false || $schema == '' ? '' : "\"{$schema}\".") . "\"$name\"";
     }
 
-    private function setSearchField($details) {
+    private function setSearchField($details)
+    {
         if ($details['schema'] != null) {
             $this->query(
-                    sprintf('SET search_path TO "%s", public', $details['schema'])
+                sprintf('SET search_path TO "%s", public', $details['schema'])
             );
         }
     }
 
-    protected function getDriverName() {
+    protected function getDriverName()
+    {
         return 'pgsql';
     }
 
     #[\Override]
-    protected function _addSchema($name) {
+    protected function _addSchema($name)
+    {
         $this->query(sprintf('CREATE SCHEMA "%s"', $name));
     }
 
     #[\Override]
-    protected function _dropSchema($name) {
+    protected function _dropSchema($name)
+    {
         $this->query(sprintf('DROP SCHEMA "%s"', $name));
     }
 
     #[\Override]
-    protected function _addTable($details) {
+    protected function _addTable($details)
+    {
         $this->query(sprintf('CREATE TABLE  %s ()', $this->buildTableName($details['name'], $details['schema'])));
     }
 
     #[\Override]
-    protected function _addView($details) {
+    protected function _addView($details)
+    {
         $this->setSearchField($details);
         $this->query(sprintf('CREATE VIEW %s AS %s', $this->buildTableName($details['name'], $details['schema']), $details['definition']));
     }
 
     #[\Override]
-    protected function _dropView($details) {
+    protected function _dropView($details)
+    {
         $this->query(sprintf('DROP VIEW %s', $this->buildTableName($details['name'], $details['schema'])));
     }
 
     #[\Override]
-    protected function _changeViewDefinition($details) {
+    protected function _changeViewDefinition($details)
+    {
         $this->setSearchField($details['to']);
         $name = $this->buildTableName($details['to']['name'], $details['to']['schema']);
         $this->query(sprintf("CREATE OR REPLACE VIEW %s AS %s", $name, $details['to']['definition']));
     }
 
     #[\Override]
-    protected function _dropTable($details) {
+    protected function _dropTable($details)
+    {
         $primaryKey = $this->query(
-                "select column_default from 
+            "select column_default from 
                 information_schema.table_constraints pk 
                 join information_schema.key_column_usage c on 
                    c.table_name = pk.table_name and 
@@ -82,17 +92,19 @@ class Postgresql extends AbstractDatabaseManipulator
         }
     }
 
-    public function describe() {
+    public function describe()
+    {
         $descriptor = new \yentu\descriptors\Postgresql($this);
         return $descriptor->describe();
     }
 
     #[\Override]
-    protected function _addColumn($details) {
+    protected function _addColumn($details)
+    {
         $this->query(
-        sprintf('ALTER TABLE %s ADD COlUMN "%s" %s', $this->buildTableName($details['table'], $details['schema']), $details['name'], $this->convertTypes(
-                    $details['type'], self::CONVERT_TO_DRIVER, $details['length']
-                )
+            sprintf('ALTER TABLE %s ADD COlUMN "%s" %s', $this->buildTableName($details['table'], $details['schema']), $details['name'], $this->convertTypes(
+                $details['type'], self::CONVERT_TO_DRIVER, $details['length']
+            )
             )
         );
         $this->_changeColumnNulls($details);
@@ -100,7 +112,8 @@ class Postgresql extends AbstractDatabaseManipulator
     }
 
     #[\Override]
-    protected function _dropColumn($details) {
+    protected function _dropColumn($details)
+    {
         $this->query(
             sprintf(
                 'ALTER TABLE %s DROP COLUMN "%s"', $this->buildTableName($details['table'], $details['schema']), $details['name']
@@ -109,7 +122,8 @@ class Postgresql extends AbstractDatabaseManipulator
     }
 
     #[\Override]
-    protected function _changeColumnDefault($details) {
+    protected function _changeColumnDefault($details)
+    {
         $details = Parameters::wrap($details, ['default']);
         $query = 'ALTER TABLE %s ALTER COLUMN "%s" ';
         if ($details['default'] != '') {
@@ -132,11 +146,12 @@ class Postgresql extends AbstractDatabaseManipulator
     }
 
     /**
-     * 
+     *
      * @param array $details
      */
     #[\Override]
-    protected function _changeColumnNulls($details) {
+    protected function _changeColumnNulls($details)
+    {
         $details = Parameters::wrap($details, ['nulls']);
         $query = 'ALTER TABLE %s ALTER COLUMN "%s" ';
 
@@ -160,103 +175,122 @@ class Postgresql extends AbstractDatabaseManipulator
     }
 
     #[\Override]
-    protected function _changeColumnName($details) {
+    protected function _changeColumnName($details)
+    {
         $this->query(
-                sprintf(
-                        'ALTER TABLE %s RENAME COLUMN "%s" TO "%s"', $this->buildTableName($details['to']['table'], $details['to']['schema']), $details['from']['name'], $details['to']['name']
-                )
+            sprintf(
+                'ALTER TABLE %s RENAME COLUMN "%s" TO "%s"', $this->buildTableName($details['to']['table'], $details['to']['schema']), $details['from']['name'], $details['to']['name']
+            )
         );
     }
 
     #[\Override]
-    protected function _addPrimaryKey($details) {
+    protected function _addPrimaryKey($details)
+    {
         $this->query(
-                sprintf(
-                        'ALTER TABLE %s ADD CONSTRAINT "%s" PRIMARY KEY ("%s")', $this->buildTableName($details['table'], $details['schema']), $details['name'], implode('","', $details['columns'])
-                )
+            sprintf(
+                'ALTER TABLE %s ADD CONSTRAINT "%s" PRIMARY KEY ("%s")', $this->buildTableName($details['table'], $details['schema']), $details['name'], implode('","', $details['columns'])
+            )
         );
     }
 
     #[\Override]
-    protected function _dropPrimaryKey($details) {
+    protected function _dropPrimaryKey($details)
+    {
         $this->query(
-                sprintf(
-                        'ALTER TABLE %s DROP CONSTRAINT "%s" CASCADE', $this->buildTableName($details['table'], $details['schema']), $details['name']
-                )
+            sprintf(
+                'ALTER TABLE %s DROP CONSTRAINT "%s" CASCADE', $this->buildTableName($details['table'], $details['schema']), $details['name']
+            )
         );
     }
 
     #[\Override]
-    protected function _addUniqueKey($details) {
+    protected function _addUniqueKey($details)
+    {
         $this->query(
-                sprintf(
-                        'ALTER TABLE %s ADD CONSTRAINT "%s" UNIQUE ("%s")', $this->buildTableName($details['table'], $details['schema']), $details['name'], implode('","', $details['columns'])
-                )
+            sprintf(
+                'ALTER TABLE %s ADD CONSTRAINT "%s" UNIQUE ("%s")', $this->buildTableName($details['table'], $details['schema']), $details['name'], implode('","', $details['columns'])
+            )
         );
     }
 
-    private function dropKeyItem($details, $type) {
+    private function dropKeyItem($details, $type)
+    {
         $this->query(
-                sprintf(
-                        'ALTER TABLE %s DROP CONSTRAINT "%s" CASCADE', $this->buildTableName($details['table'], $details['schema']), $details['name']
-                )
+            sprintf(
+                'ALTER TABLE %s DROP CONSTRAINT "%s" CASCADE', $this->buildTableName($details['table'], $details['schema']), $details['name']
+            )
         );
 
         //$this->dropTableItem($details, $type);
     }
 
     #[\Override]
-    protected function _dropUniqueKey($details) {
+    protected function _dropUniqueKey($details)
+    {
         $this->dropKeyItem($details, 'unique_keys');
     }
 
     #[\Override]
-    protected function _addAutoPrimaryKey($details) {
+    protected function _addAutoPrimaryKey($details)
+    {
         $sequence = $this->buildTableName("{$details['table']}_{$details['column']}_seq", $details['schema']);
         $this->query("CREATE SEQUENCE $sequence");
         $this->query(
-                sprintf(
-                        'ALTER TABLE %s ALTER COLUMN "%s" SET DEFAULT nextval(\'%s\')', $this->buildTableName($details['table'], $details['schema']), $details['column'], $sequence
-                )
+            sprintf(
+                'ALTER TABLE %s ALTER COLUMN "%s" SET DEFAULT nextval(\'%s\')', $this->buildTableName($details['table'],
+                $details['schema']), $details['column'], $sequence
+            )
         );
     }
 
     #[\Override]
-    protected function _dropAutoPrimaryKey($details) {
+    protected function _dropAutoPrimaryKey($details)
+    {
         $sequence = $this->buildTableName("{$details['table']}_{$details['column']}_seq", $details['schema']);
         $this->query(
-                sprintf(
-                        'ALTER TABLE %s ALTER COLUMN "%s" SET DEFAULT NULL', $this->buildTableName($details['table'], $details['schema']), $details['column'], $sequence
-                )
+            sprintf(
+                'ALTER TABLE %s ALTER COLUMN "%s" SET DEFAULT NULL', $this->buildTableName($details['table'],
+                $details['schema']), $details['column'], $sequence
+            )
         );
         $this->query("DROP SEQUENCE IF EXISTS $sequence");
     }
 
     #[\Override]
-    protected function _addForeignKey($details) {
+    protected function _addForeignKey($details)
+    {
         $this->query(
-                sprintf(
-                        'ALTER TABLE %s ADD CONSTRAINT "%s" FOREIGN KEY ("%s") REFERENCES %s ("%s") MATCH FULL ON DELETE %s ON UPDATE %s', $this->buildTableName($details['table'], $details['schema']), $details['name'], implode('","', $details['columns']), $this->buildTableName($details['foreign_table'], $details['foreign_schema']), implode('","', $details['foreign_columns']), $details['on_delete'] == '' ? 'NO ACTION' : $details['on_delete'], $details['on_update'] == '' ? 'NO ACTION' : $details['on_update']
-                )
+            sprintf(
+                'ALTER TABLE %s ADD CONSTRAINT "%s" FOREIGN KEY ("%s") REFERENCES %s ("%s") MATCH FULL ON DELETE %s ON UPDATE %s',
+                $this->buildTableName($details['table'], $details['schema']), $details['name'],
+                implode('","', $details['columns']), $this->buildTableName($details['foreign_table'], $details['foreign_schema']),
+                implode('","', $details['foreign_columns']),
+                $details['on_delete'] == '' ? 'NO ACTION' : $details['on_delete'], $details['on_update'] == '' ? 'NO ACTION' : $details['on_update']
+            )
         );
     }
 
     #[\Override]
-    protected function _dropForeignKey($details) {
+    protected function _dropForeignKey($details)
+    {
         $this->dropKeyItem($details, 'foreign_keys');
     }
 
     #[\Override]
-    protected function _addIndex($details) {
+    protected function _addIndex($details)
+    {
         $this->query(
-                sprintf(
-                        'CREATE INDEX %s "%s" ON %s ("%s")', $details['unique'] ? 'UNIQUE' : '', $details['name'], $this->buildTableName($details['table'], $details['schema']), implode('", "', $details['columns'])
-                )
+            sprintf(
+                'CREATE INDEX %s "%s" ON %s ("%s")', $details['unique'] ? 'UNIQUE' : '', $details['name'],
+                $this->buildTableName($details['table'], $details['schema']), implode('", "', $details['columns'])
+            )
         );
     }
 
     #[\Override]
-    protected function _dropIndex($details) {
+    protected function _dropIndex($details)
+    {
         $this->query(sprintf('DROP INDEX %s', $this->buildTableName($details['name'], $details['schema'])));
     }
 
@@ -267,7 +301,8 @@ class Postgresql extends AbstractDatabaseManipulator
     }
 
     #[\Override]
-    public function convertTypes($type, $direction, $length = null) {
+    public function convertTypes($type, $direction, $length = null)
+    {
         $types = array(
             'integer' => 'integer',
             'bigint' => 'bigint',
@@ -302,9 +337,11 @@ class Postgresql extends AbstractDatabaseManipulator
     }
 
     #[\Override]
-    protected function _changeTableName($details) {
+    protected function _changeTableName($details)
+    {
         $this->query(sprintf(
-                "ALTER TABLE %s RENAME TO %s", $this->buildTableName($details['from']['name'], $details['from']['schema']), $this->buildTableName($details['to']['name'], false)
+                "ALTER TABLE %s RENAME TO %s", $this->buildTableName($details['from']['name'],
+                $details['from']['schema']), $this->buildTableName($details['to']['name'], false)
             )
         );
     }
