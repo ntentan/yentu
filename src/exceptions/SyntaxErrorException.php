8c9dd32b612147794b9f736bb5db8d05c6d82120
James Ekow Abaka Ainooson <jainooson@gmail.com>
2025-01-12 12:19:34 -0600

    A little cleanup

--- src/exceptions/SyntaxErrorException.php
+++ src/exceptions/SyntaxErrorException.php
@@ -2,8 +2,6 @@
 
 namespace yentu\exceptions;
 
-use yentu\Yentu;
-
 /**
  * Reports syntax errors in migration scripts with an attempt to show the actual line where error ocurred.
  */
@@ -11,7 +9,6 @@ class SyntaxErrorException extends YentuException {
 
     public function __construct(string $message, string $homePath) 
     {
-
         parent::__construct($message);
         foreach ($this->getTrace() as $item) {
             if (realpath($homePath . DIRECTORY_SEPARATOR . "migrations") === dirname($item['file'] ?? '')) {
@@ -20,5 +17,4 @@ class SyntaxErrorException extends YentuException {
             }
         }
     }
-
 }
